ba35d11a1eca95a6bf7dc74c2fa1cc94372e69b5
Alexey Fedorenko <fedorenksei@gmail.com>
2019-03-17 21:48:05 +0300

    общелексемные поля
    смотрю, какие поля и сколько раз встречаются перед полем \ms (значение) в тех полях, где есть \ms

--- reading_dict.py
+++ reading_dict.py
@@ -1,10 +1,26 @@
 def main():
     dictFile = open('blowo12.txt', 'r', encoding='utf8')
-    lexemes = dictFile.read().split('\n\n'): #разделяю на лексемы
+    lexemes = dictFile.read().split('\n\n') #разделяю на лексемы
     del lexemes[0]# удаляю нулевой элемент — строку с названием словаря
+    common = {}
+    i = 0
     for lexeme in lexemes:
-        readLexeme(lexeme)
+        if '\n\ms ' in lexeme:
+            i += 1
+            for field in commonFields(lexeme):
+                if field not in common:
+                    common[field] = 1
+                else:
+                    common[field] += 1
+    print(common)
     dictFile.close()
+
+def commonFields(lexeme):
+    result = []
+    commonLines = lexeme.split('\n\ms ')[0]
+    for line in commonLines.split('\n'):
+        result.append(line.split(' ')[0])
+    return result
 
 if __name__ == '__main__':
     main()
