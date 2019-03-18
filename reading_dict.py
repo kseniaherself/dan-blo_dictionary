200dda86e2d6c4976f719fbfcd2afc4694c626bd
Alexey Fedorenko <fedorenksei@gmail.com>
2019-03-18 10:47:07 +0300

    подсчет полей и в шапках, и в значениях
    словарь, где ключ - поле, значение - [кол-во в шапках, кол-во в значениях]

--- reading_dict.py
+++ reading_dict.py
@@ -2,25 +2,50 @@ def main():
     dictFile = open('blowo12.txt', 'r', encoding='utf8')
     lexemes = dictFile.read().split('\n\n') #разделяю на лексемы
     del lexemes[0]# удаляю нулевой элемент — строку с названием словаря
-    common = {}
+    fieldList = {} #списки полей из шапок и значений
     i = 0
-    for lexeme in lexemes:
+    for id, lexeme in enumerate(lexemes, 1):
+        readLexeme(lexeme, id)
+        ## делаю список полей из шапок
         if '\n\ms ' in lexeme:
             i += 1
-            for field in commonFields(lexeme):
-                if field not in common:
-                    common[field] = 1
+            for field in getFields(lexeme)[0]:
+                if field not in fieldList:
+                    fieldList[field] = [1, 0]
                 else:
-                    common[field] += 1
-    print(common)
+                    fieldList[field][0] += 1
+            for field in getFields(lexeme)[1]:
+                if field not in fieldList:
+                    fieldList[field] = [0, 1]
+                else:
+                    fieldList[field][1] += 1
+    print(fieldList)
     dictFile.close()
 
-def commonFields(lexeme):
-    result = []
-    commonLines = lexeme.split('\n\ms ')[0]
+def readLexeme(lexeme, id):
+    inLex = []
+    inLex.insert(id, 0)
+    for line in lexeme.split('\n'):
+        divBySpaces = line.split(' ')
+        name = divBySpaces[0]
+        content = ' '.join(divBySpaces[1:len(divBySpaces)-1])
+        if name == '\le':
+            inLex.insert(1, content)
+        if name == '\ms':
+            ms = content
+        #if name == '\dfe':
+
+def getFields(lexeme):
+    commonFields = []
+    msFields = []
+    divByMs = lexeme.split('\n\ms ')
+    commonLines = divByMs[0]
+    msLines = '\n\ms '.join(divByMs[1:len(divByMs)-1])
     for line in commonLines.split('\n'):
-        result.append(line.split(' ')[0])
-    return result
+        commonFields.append(line.split(' ')[0])
+    for line in msLines.split('\n'):
+        msFields.append(line.split(' ')[0])
+    return [commonFields, msFields]
 
 if __name__ == '__main__':
     main()
